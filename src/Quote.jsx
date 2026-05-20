0891c72fb4b73f6b60286e8b71bd0dcb389ae703
Ben Brown <benbrown3882@gmail.com>
2026-05-19 22:38:15 -0700

    added history

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -4,12 +4,14 @@ import Button from "./Button";
 export default function Quote() {
   const [quote, setQuote] = useState("Get a daily quote from Zen Quotes");
   const [buttonText, setButtonText] = useState("Get Today's Quote");
+  const [quoteHistory, setQuoteHistory] = useState([]);
   // 1. We mark the function as 'async' so we can use 'await' inside it
   async function getDailyQuote() {
     try {
       // 2. The 'await' pauses this function until the network request finishes
-      const response = await fetch("https://quotes-api.habaneropress.com/api/quote");
-      
+      const response = await fetch(
+        "https://quotes-api.habaneropress.com/api/quote",
+      );
 
       // 3. Always check if the response is okay (status 200)
       if (!response.ok) {
@@ -28,7 +30,8 @@ export default function Quote() {
       const newQuote = `${quote} - ${author}`;
 
       // You could then update your HTML here:
-      // document.getElementById('quote-box').innerText = quote;
+      setQuoteHistory((prevHistory) => [newQuote, ...prevHistory]);
+
       setQuote(newQuote);
       setButtonText("Get another quote!");
     } catch (error) {
@@ -40,12 +43,22 @@ export default function Quote() {
   // Call the function
   return (
     <>
-      <main className="container px-5 py-24 mx-auto flex">
+      <main className="container px-5 py-24 mx-auto flex flex-col">
         <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
           <blockquote className="grow sm:pr-16 text-2xl font-medium title-font text-gray-900">
             {quote}
           </blockquote>
           <Button getQuote={getDailyQuote} buttonText={buttonText} />
+        </div>
+
+        <div className="mt-10 lg:w-2/3 mx-auto">
+          <ul className="list-disc pl-5 space-y-2">
+            {quoteHistory.slice(1).map((item, index) => (
+              <li key={index} className="text-gray-600 italic">
+                {item}
+              </li>
+            ))}
+          </ul>
         </div>
       </main>
     </>
